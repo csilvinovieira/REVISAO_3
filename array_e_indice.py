cc601ce02ec528f75b53f0a53cb7d4dd86f64ac8
Clenio Silvino Vieira <clenio.vieira@htell.grupohpar.corp>
2024-04-03 20:50:02 -0300

    Resolve questao 3

--- array_e_indice.py
+++ array_e_indice.py
@@ -10,3 +10,17 @@ def imprimir_primeiro_ultimo_nome():
 
 # Chamando a função
 imprimir_primeiro_ultimo_nome()
+
+
+def imprimir_segundo_terceiro_nome():
+    # Criando um array com quatro nomes diferentes
+    nomes = ['João', 'Maria', 'Pedro', 'Beltrano']
+
+    # Imprimindo o segundo nome (índice 1)
+    print(f"2-{nomes[1]}")
+
+    # Imprimindo o terceiro nome (índice 2)
+    print(f"3-{nomes[2]}")
+
+# Chamando a função
+imprimir_segundo_terceiro_nome()
