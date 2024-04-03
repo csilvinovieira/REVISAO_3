81ea46cc28b2b8ef7ce05c043b72869ddc8e2673
Clenio Silvino Vieira <clenio.vieira@htell.grupohpar.corp>
2024-04-03 20:41:20 -0300

    Resolve questao 1

---
+++ array_e_indice.py
@@ -0,0 +1,12 @@
+def imprimir_primeiro_ultimo_nome():
+    # Criando um array com quatro nomes diferentes
+    nomes = ['João', 'Maria', 'Pedro', 'Beltrano']
+
+    # Imprimindo o primeiro nome
+    print(f"1-{nomes[0]}")
+
+    # Imprimindo o último nome
+    print(f"4-{nomes[-1]}")
+
+# Chamando a função
+imprimir_primeiro_ultimo_nome()
